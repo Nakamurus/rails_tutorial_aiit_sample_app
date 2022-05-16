f7863b804c065e863187d75c134529a07606a559
nakamurus <3872mn@gmail.com>
2022-05-16 08:03:19 +0000

    implement remember me

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
+
   before_save { email.downcase! }
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
@@ -9,8 +11,30 @@ class User < ApplicationRecord
   validates :password, presence: true, length: {minimum: 6 }
   
   def User.digest(string)
+    # return hash value of an argument
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
+  end
+  
+  def User.new_token
+    # returns a random token
+    SecureRandom.urlsafe_base64
+  end
+  
+  def remember
+    # map a user to a database for a persistent session
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+  
+  def authenticated?(remember_token)
+    # returns true if an passed token matches with the digest
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+  
+  def forget
+    update_attribute(:remember_digest, nil)
   end
 end
